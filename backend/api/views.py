9b58b9097f4653e22af6bd4749f7a60c3944f954
vipin <vipinkumarlamba55@gmail.com>
2024-02-23 15:36:05 +0530

    Rest framework apiview and Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,9 +3,13 @@ from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from products.models import Product
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     product = Product.objects.all().order_by("?").first()
     data = {}
     if product:
         data = model_to_dict(product, fields=["id", "title", "content", "price"])
-    return JsonResponse(data)
+    return Response(data)
--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
